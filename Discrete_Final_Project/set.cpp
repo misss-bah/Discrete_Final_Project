39ace4916d3769618541d2f0c62d2dfa7840da4f
Wabil-Rehman <wabilrehman2626@gmail.com>
2025-11-22 23:28:05 +0500

    weddeq

---
+++ Discrete_Final_Project/set.cpp
@@ -0,0 +1,14 @@
+//#include "Sets.h"
+//using namespace std;
+//
+//SetModule::SetModule() {}
+//
+//void SetModule::addStudent(const string& student) {
+//    students.add(student);
+//    cout << "Student added to set: " << student << endl;
+//}
+//
+//void SetModule::addCourse(const string& course) {
+//    courses.add(course);
+//    cout << "Course added to set: " << course << endl;
+//}
